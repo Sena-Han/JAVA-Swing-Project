734e8e7fca7a59f51ebf6781ddd0cbc6fc1db7d9
SenaHan <hsysn1@naver.com>
2023-11-12 16:17:34 +0900

    GamePanel 수정 및 MapObject, Screen 자바 파일 추가

---
+++ 2023-JAVA-Swing-Project/src/inside/MapObject.java
@@ -0,0 +1,20 @@
+package inside;
+
+import javax.swing.ImageIcon;
+
+public class MapObject {
+    private ImageIcon backScreenImg;
+    
+    public MapObject() {
+    	//기본생성
+    }
+    
+    public ImageIcon getbackScreenImg() {
+        return backScreenImg;
+    }
+
+    public void setBackScreenImg(ImageIcon backScreenImg) {
+        this.backScreenImg = backScreenImg;
+    }
+
+}
--- 2023-JAVA-Swing-Project/src/panels/GamePanel.java
+++ 2023-JAVA-Swing-Project/src/panels/GamePanel.java
@@ -1,20 +1,34 @@
 package panels;
 
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Color;
+import java.awt.CardLayout;
 import java.awt.AlphaComposite;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import inside.MapObject;
 import inside.Obstacle;
 import inside.Score;
+import inside.Screen;
 import inside.Vava;
+import main.Main;
 
 public class GamePanel extends JPanel {
+	
+	//screen
+	private ImageIcon backScreenImg;
+	private ImageIcon backScreenImg2;
+	private ImageIcon backScreenImg3;
+	private ImageIcon backScreenImg4;
 
 	// 더블 버퍼링 이미지
 	private Image bufferImage;
@@ -36,14 +50,41 @@ public class GamePanel extends JPanel {
 	private AlphaComposite alphaComposite; // 투명도 관련 변수
 	
 	private int sumScore = 0; // 결과점수 변수 (누적 score)
+	
+	private boolean fadeOn = false;
 
 	Vava v1; // 바바 객체
+	
+	Screen back11;
+	Screen back12;
+	Screen back21;
+	Screen back22;
+	
+	Color screenFade;
+	
+	JFrame superFrame;
+	CardLayout cardLayout;
+	Main main;
+	
+	public GamePanel(JFrame superFrame, CardLayout cardLayout, Object o) {
+		this.superFrame = superFrame;
+		this.cardLayout = cardLayout;
+		this.main = (Main)o;
+	}
 	
 	// 화면을 그림
 	@Override
 	protected void paintComponent(Graphics g) {
 
-		Graphics2D g2 = (Graphics2D) bufferg; 
+		Graphics2D g2 = (Graphics2D) bufferg;
+		
+		if (bufferg==null) {
+			bufferImage = createImage(this.getWidth(), this.getHeight());
+			if (bufferImage == null) System.out.println("더블 버퍼링용 오프 스크 생성 실패");
+			else bufferg = bufferImage.getGraphics();
+		}
+		
+		super.paintComponent(bufferg);
 
 		// 장애물을 그림
 		for (int i = 0; i < obstacleList.size(); i++) {
@@ -56,6 +97,7 @@ public class GamePanel extends JPanel {
 						tempObstacle.getHeight(), null);
 			}
 		}
+		
 		// score(학점)를 그림
 		for (int i = 0; i < scoreList.size(); i++) {
 
@@ -74,6 +116,16 @@ public class GamePanel extends JPanel {
 				alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) 255 / 255);
 				g2.setComposite(alphaComposite);
 			}
+		}
+		
+		bufferg.drawImage(back11.getImage(), back11.getX(), 0, back11.getWidth(), back11.getHeight(), null);
+		bufferg.drawImage(back12.getImage(), back12.getX(), 0, back12.getWidth(), back12.getHeight(), null);
+		bufferg.drawImage(back21.getImage(), back21.getX(), 0, back21.getWidth(), back21.getHeight(), null);
+		bufferg.drawImage(back22.getImage(), back22.getX(), 0, back22.getWidth(), back22.getHeight(), null);
+		
+		if (fadeOn) {
+			bufferg.setColor(screenFade);
+			bufferg.fillRect(0, 0, this.getWidth(), this.getHeight());
 		}
 
 		// 버퍼 이미지를 화면에 출력
