93a2bac547db23cee47d610a720f396aee8ce437
SenaHan <hsysn1@naver.com>
2023-11-19 20:07:00 +0900

    주원이 코드 추가

--- 2023-JAVA-Swing-Project/src/main/Main.java
+++ 2023-JAVA-Swing-Project/src/main/Main.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -13,28 +14,31 @@ import javax.swing.JOptionPane;
 
 import panels.IntroPanel;
 import panels.StoryPanel;
-import main.ListenAdapter;
+//import main.ListenAdapter;
 
 
-public class Main extends ListenAdapter {
+public class Main extends MouseAdapter {
 	
 	private JFrame frame;
 	
-	private IntroPanel introPanel; // 게임의 인트로
+	private IntroPanel introPanel; // 게임 시작 화면 
 	private StoryPanel storyPanel; //바바 스토리
 	
 	private CardLayout cardLayout;
 
 	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
 				try {
 					Main window = new Main();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
-				}
+					}
 			}
 		});
 	}
@@ -43,9 +47,10 @@ public class Main extends ListenAdapter {
 		initialize();
 	}
 	
-	private void initialize() {
+	private void initialize() 
+	{
 		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 500); // 창 사이즈 (100,100좌표는 아래의 frame.setLocationRelativeTo(null) 때문에 의미가 없어진다)
+		frame.setBounds(100, 100, 1080, 720); // 창 사이즈 (100,100좌표는 아래의 frame.setLocationRelativeTo(null) 때문에 의미가 없어진다)
 		frame.setLocationRelativeTo(null); // 창을 화면 중앙에 배치
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 엑스버튼을 누르면 종료
 		
@@ -62,7 +67,7 @@ public class Main extends ListenAdapter {
 		// 프레임에 패널들을 추가한다.(카드 레이아웃을 위한 패널들)
 		frame.getContentPane().add(introPanel, "intro");
 		frame.getContentPane().add(storyPanel, "story");
-		}
+	}
 	
 	@Override
 	public void mousePressed(MouseEvent e) { // mouseClicked로 변경가능
@@ -75,8 +80,6 @@ public class Main extends ListenAdapter {
 			cardLayout.show(frame.getContentPane(), "story"); // select패널을 카드레이아웃 최상단으로 변경
 			storyPanel.requestFocus(); // 리스너를 select패널에 강제로 줌
 			
-			
-		} 				
+		}
 	}
-
 }
